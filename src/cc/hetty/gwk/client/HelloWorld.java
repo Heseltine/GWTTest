9978d0ef34a6940c49ad67a7b7609642d21debda
hwei <hwei@talend.com>
2015-07-07 11:43:22 +0800

    Initial commit

---
+++ src/cc/hetty/gwk/client/HelloWorld.java
@@ -0,0 +1,12 @@
+package cc.hetty.gwk.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class HelloWorld implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        Window.alert("Hello, World!");
+    }
+}
